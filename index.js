3767779b848a9bfbbc3d5b5e4768a2e5a10b04c0
Fabio Beneditto <fabio.beneditto@azion.com>
2022-02-23 17:46:03 -0300

    Change running port for Heroku

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || port, '0.0.0.0', () => {
-  console.log("Server is running.");
+app.listen(port, () => {
+  console.log(`Our app is running on port ${ port }`);
 });
