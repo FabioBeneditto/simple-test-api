73bea0b7058bed44f3302fd8be5d8d09284564cc
Fabio Beneditto <fabio.beneditto@azion.com>
2022-02-24 09:29:24 -0300

    Including /API route (v1.1)

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  // res.send('Hello World!')
-  res.json(req)
+  res.send('Hello World!')
+  // res.json(req)
 })
 
 app.post('/', (req, res) => {
